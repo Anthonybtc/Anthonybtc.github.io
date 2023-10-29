5775ebb29e76f6250e36e62af93447ce24a04b90
Anthonybtc <149034705+Anthonybtc@users.noreply.github.com>
2023-10-28 22:14:34 -0400

    1st

---
+++ ticker.js
@@ -0,0 +1,34 @@
+async function getCryptoPrice(endpoint) {
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return parseFloat(data.price);
+    } catch (error) {
+        console.error("Error fetching price:", error);
+        return "N/A";
+    }
+}
+
+async function updateTickers() {
+    const bitcoinTicker = document.getElementById("bitcoin-price");
+    const litecoinTicker = document.getElementById("litecoin-price");
+    const dogecoinTicker = document.getElementById("dogecoin-price");
+    const cardanoTicker = document.getElementById("cardano-price")
+
+    const bitcoinPrice = await getCryptoPrice("https://api.pro.coinbase.com/products/BTC-USD/ticker");
+    bitcoinTicker.textContent = isNaN(bitcoinPrice) ? "N/A" : `$${bitcoinPrice.toFixed(2)}`;
+
+    const litecoinPrice = await getCryptoPrice("https://api.pro.coinbase.com/products/LTC-USD/ticker");
+    litecoinTicker.textContent = isNaN(litecoinPrice) ? "N/A" : `$${litecoinPrice.toFixed(2)}`;
+
+    const dogecoinPrice = await getCryptoPrice("https://api.pro.coinbase.com/products/DOGE-USD/ticker");
+    dogecoinTicker.textContent = isNaN(dogecoinPrice) ? "N/A" : `$${dogecoinPrice.toFixed(5)}`;
+
+    const cardanoPrice = await getCryptoPrice("https://api.pro.coinbase.com/products/ADA-USD/ticker");
+    cardanoTicker.textContent = isNaN(cardanoPrice) ? "N/A" : `$${cardanoPrice.toFixed(5)}`;
+
+    setTimeout(updateTickers, 1000); // Update every second
+}
+
+updateTickers();
+
